6b54b46ae5691586cdc641e838babe79102fa404
vijaysoren <37494793+vijaysoren@users.noreply.github.com>
2024-02-06 16:13:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const menuicon = document.getElementById("hamburgericon");
+
+function showmenu() {
+    //alert("Clicked");
+    var navbar = document.getElementById("navbar");
+
+    navbar.style.height = "auto";
+    document.querySelector('a').style.display = "block";
+   
+}
